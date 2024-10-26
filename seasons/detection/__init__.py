69c4e38fcf114e0ffc134448cad10506e1847f75
Stephen Ezin <iamstephenezin@gmail.com>
2024-10-26 14:33:46 +0100

    Add plot seasonality functionality amd correct typo im detection init

--- seasons/detection/__init__.py
+++ seasons/detection/__init__.py
@@ -4,4 +4,4 @@ Detection methods for seasonality in time series data.
 
 from.brute_force import brute_force_seasonality
 from.fft import fft_seasonality
-from welch import welch_seasonality
+from.welch import welch_seasonality
