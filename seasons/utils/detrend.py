fix: seasonal effect estimation; cartesian product computation; trend estimation

seasons/utils/detrend.py
@@ -3,7 +3,7 @@ from scipy.stats import bartlett, f_oneway
 from statsmodels.api import add_constant, OLS
 from PyEMD import EMD
 
-def remove_trend(data, seasonality_type: str = 'auto', use_linear_reg: bool = True):
+def remove_trend(data, seasonality_type: str = 'auto', use_linear_reg: bool = False):
     """
     Stationarize a time series based on the specified seasonality type.
 
@@ -35,8 +35,11 @@ def remove_trend(data, seasonality_type: str = 'auto', use_linear_reg: bool = Tr
 
 def _estimate_and_validate_trend(data, use_linear_reg):
     trend = _estimate_trend(data, use_linear_reg)
-    while _has_seasonality(trend):
-        trend = _estimate_trend(trend, use_linear_reg)
+    if use_linear_reg == False:
+        second_trend = _estimate_trend(trend, use_linear_reg)
+        while _has_seasonality(trend - second_trend):
+            trend = _estimate_trend(trend, use_linear_reg)
+            second_trend = _estimate_trend(trend, use_linear_reg)
     return trend
 
 

seasons/utils/estimate_effect.py
@@ -7,11 +7,15 @@ import matplotlib.pyplot as plt
 def compute_seasonal_effects(
     data: np.ndarray,
     seasons: np.ndarray,
-    alpha: float = 0.05,
+    already_detrended: bool,
+    trend = None,
+    detrended = None,
     seasonality_type: str = 'auto',
+    alpha: float = 0.05,
     display_plot: bool = False,
     return_effects: bool = True,
-    use_linear_reg: bool = False
+    use_linear_reg: bool = False,
+    normalize: bool = False,
 ) -> dict:
     """
     Plot seasonal components of a time series.
@@ -19,7 +23,7 @@ def compute_seasonal_effects(
     Args:
     - series (np.ndarray): Input time series.
     - seasons (np.ndarray): Array of seasonal periods.
-    - seasonality_type (str): Type of seasonality ('additive' or 'ultiplicative' or 'auto'). Defaults to 'auto'.
+    - seasonality_type (str): Type of seasonality ('additive' or 'multiplicative' or 'auto'). Defaults to 'auto'.
     - repeat_seasons (bool, optional): Whether to repeat seasonal effects to match series length. Defaults to False.
     - display_plot (bool, optional): Whether to display the plot. Defaults to False.
     - return_effects (bool, optional): Whether to return a dict of the seasons and their effects
@@ -29,12 +33,12 @@ def compute_seasonal_effects(
     seasons = np.sort(seasons)
 
     # Detrend
-    trend, detrended, seasonality_type = remove_trend(data=data, seasonality_type=seasonality_type, use_linear_reg=use_linear_reg)
+    if not already_detrended:
+        trend, detrended, seasonality_type = remove_trend(data=data, seasonality_type=seasonality_type, use_linear_reg=use_linear_reg)
     N = len(detrended)
     # estimating effects
-    seasons_effects = _confidence_interval(data=detrended, seasonal_periods=seasons, alpha=alpha)
-    print(f"After estimation. {seasons_effects=}")
-
+    seasons_effects = _confidence_interval(data=detrended, seasonal_periods=seasons, normalize=True, seasonality_type=seasonality_type, alpha=alpha)
+    
     if display_plot:
         _plot_seasonal_components(
             seasonal_components=seasons_effects,
@@ -59,17 +63,17 @@ def _plot_seasonal_components(
     N_FIG = len(seasonal_components) + 4
     fig, ax = plt.subplots(nrows=N_FIG, ncols=1, figsize=(16, 6*N_FIG))
 
-    ax[0].plot(data, label="Original series")
-    ax[0].plot(trend, label="Estimated trend")
+    ax[0].plot(range(1, len(data) + 1), data, label="Original series")
+    ax[0].plot(range(1, len(trend) + 1), trend, label="Estimated trend")
     ax[0].legend()
-    ax[1].plot(detrended, label=f"Detrended series, Seas. type={seasonality_type}")
+    ax[1].plot(range(1, len(detrended) + 1), detrended, label=f"Detrended series, Seas. type={seasonality_type}")
     ax[1].legend()
 
-    seasonal_sums_lengths = sum([len(component["effect"]) for season, component in seasonal_components.items()])
+    #seasonal_sums_lengths = sum([len(component["effect"]) for season, component in seasonal_components.items()])
     if seasonality_type == 'additive':
-        total_seasonal_effect = np.zeros(seasonal_sums_lengths)
+        total_seasonal_effect = np.zeros(len(data))
     else: 
-        total_seasonal_effect = np.ones(seasonal_sums_lengths)
+        total_seasonal_effect = np.ones(len(data))
 
     for i, (season, component) in enumerate(seasonal_components.items()):
         effects = component["effect"]
@@ -80,9 +84,9 @@ def _plot_seasonal_components(
             total_seasonal_effect *= effects_to_add
         conf_intervals = component["confidence_interval"]        
         # Plot effects
-        ax[i+4].plot(effects, marker='o', label=f"Effect ({season})")
+        ax[i+4].plot(range(1, len(effects) + 1), effects, marker='o', label=f"Effect ({season})")
         ax[i+4].fill_between(
-            range(len(effects)),
+            range(1, len(effects) + 1),
             [interval[0] for interval in conf_intervals],
             [interval[1] for interval in conf_intervals],
             color="grey",
@@ -91,14 +95,14 @@ def _plot_seasonal_components(
         )
         ax[i+4].legend()
 
-    ax[2].plot(total_seasonal_effect, label=f"Total Seasonal Effect")
+    ax[2].plot(range(1, len(total_seasonal_effect) + 1), total_seasonal_effect, label=f"Total Seasonal Effect")
     ax[2].legend()
-    total_seasonal_effect_to_add = _repeat_array_until_length(total_seasonal_effect, len(detrended))
+    #total_seasonal_effect_to_add = _repeat_array_until_length(total_seasonal_effect, len(detrended))
     if seasonality_type == 'additive':
-        residuals = detrended - total_seasonal_effect_to_add
+        residuals = detrended - total_seasonal_effect
     else:
-        residuals = detrended / total_seasonal_effect_to_add
-    ax[3].plot(residuals, label=f"Residuals", marker='o')
+        residuals = detrended / total_seasonal_effect
+    ax[3].plot(range(1, len(residuals) + 1), residuals, label=f"Residuals", marker='o')
     ax[3].legend()
 
     # Render plot
@@ -143,20 +147,29 @@ def _reshape_to_2d(array, rows):
 def _confidence_interval(
     data: np.ndarray,
     seasonal_periods: list,
+    normalize: bool = False,
+    seasonality_type: str = None,
     alpha: float = 0.05,
 ) -> dict:
     """
     data: detreded time series
     """
     X = _generate_design_matrix(len(data), seasonal_periods=seasonal_periods)
-    X = add_constant(X)
-    print(X)
+    #X = add_constant(X)
     model = OLS(data, X)
     model = model.fit()
-    const, coefficients = model.params[0], model.params[1:]
-    print("After regression")
-    print(model.summary())
-    confidence_int = model.conf_int(alpha)[1:]
+    coefficients = model.params
+    confidence_int = model.conf_int(alpha)
+    # normalize
+    if normalize:
+        min_coefficients = np.min(coefficients)
+        if seasonality_type == 'additive':
+            coefficients += np.abs(min_coefficients)
+            confidence_int = np.array([[lower + np.abs(min_coefficients), upper + np.abs(min_coefficients)] for (lower, upper) in confidence_int])
+        elif seasonality_type == 'multiplicative':
+            if min_coefficients != 0:
+                coefficients *= (1/min_coefficients)
+                confidence_int = np.array([[lower * (1/min_coefficients), upper * (1/min_coefficients)] for (lower, upper) in confidence_int])
 
     # Initialize output dictionary
     seasonal_components = {}
@@ -166,10 +179,6 @@ def _confidence_interval(
     for period, period_name in enumerate(seasonal_periods, start=1):
         period_coeffs = coefficients[coeff_index:coeff_index + period_name]
         period_conf_int = confidence_int[coeff_index:coeff_index + period_name]
-        # Adjust coefficients for last season (if applicable)
-        if (period == len(seasonal_periods)):
-            period_coeffs += const
-            period_conf_int = np.array([(lower + const, upper + const) for (lower, upper) in period_conf_int ])
         
         # Store results in dictionary
         seasonal_components[f"s{period_name}"] = {